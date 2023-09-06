5c9e8647a9260df8d23d24cd3cefe624ce6fcd1c
evafigueiredo8 <144215024+evafigueiredo8@users.noreply.github.com>
2023-09-06 16:35:27 +0200

    v2 ex2_lettre
    v2 avec le tableau poids

--- ex2_simulationdaffranchissementlettre.py
+++ ex2_simulationdaffranchissementlettre.py
@@ -3,45 +3,47 @@ import math
 tarif = 0 #float
 testtype = True #bool
 while testtype :
-    typelettre = input("Quel est le type de votre lettre parmi 'verte', 'prioritaire' ou 'éco-pli' ?" )
-    if typelettre == "verte" or "prioritaire" or "éco-pli" :
+    typelettre = input("Quel est le type de votre lettre parmi 'v' pour verte, 'p pour prioritaire ou 'e' pour eco-pli ?").lower()
+    if typelettre == "v" or "p" or "e" :
         testtype = False
     else :
         testtype == True
 
+POIDS = [20,100,250,500,1000,3000]
+poids = input("Quel est le poids de votre lettre en gramme ?" ).math.ceil() #int, converti en entier supérieur le + petit
 
-poids = input("Quel est le poids de votre lettre en gramme ?" ) #int
-
-if typelettre == "verte" :
-    if poids < 20 :
+if typelettre == "v" :
+    if poids < POIDS(0) :
         tarif = 1.16
-    elif poids < 100 :
+    elif poids < POIDS(1) :
         tarif = 2.32
-    elif poids < 250 :
+    elif poids < POIDS(2) :
         tarif = 4.00
-    elif poids < 500 :
+    elif poids < POIDS(3) :
         tarif = 6.00
-    elif poids < 1000 :
+    elif poids < POIDS(4) :
         tarif = 7.50
-    elif poids < 3000 :
+    elif poids < POIDS(5) :
         tarif = 10.50
 
-if typelettre == "prioritaire" :
-    if poids < 20 :
+if typelettre == "p" :
+    if poids < POIDS(0) :
         tarif = 1.43
-    elif poids < 100 :
+    elif poids < POIDS(1) :
         tarif = 2.86
-    elif poids < 250 :
+    elif poids < POIDS(2) :
         tarif = 5.26
-    elif poids < 500 :
+    elif poids < POIDS(3) :
         tarif = 7.89
-    elif poids < 3000 :
+    elif poids < POIDS(5) :
         tarif = 11.44
 
-if typelettre == "éco-pli" :
-    if poids < 20 :
+if typelettre == "e" :
+    if poids < POIDS(0) :
         tarif = 1.14
-    elif poids < 100 :
+    elif poids < POIDS(1) :
         tarif = 2.28
-    elif poids < 250 :
+    elif poids < POIDS(2) :
         tarif = 3.92
+
+print ("Le tarif pour votre lettre est de", tarif,"€.")
