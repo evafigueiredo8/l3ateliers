31fe54ced83249d4cf8d51a0c8da81c68216eadb
evafigueiredo8 <144215024+evafigueiredo8@users.noreply.github.com>
2023-09-07 09:49:36 +0200

    v1 exercice 1 atelier 1

---
+++ atelier1_ex1.py
@@ -0,0 +1,32 @@
+import math
+
+
+
+def message_imc (imc) :
+
+    IMC = [16.5,18.5,25,30,35,40]
+    INTERPRETATION = ["Dénutrition ou famine","Maigreur","Corpulence normale","Surpoids","Obésité modérée","Obésité sévère","Obésité morbide"]
+
+    if imc < IMC[0] :
+        interpretation = INTERPRETATION[0]
+    elif imc < IMC[1] :
+        interpretation = INTERPRETATION[1]
+    elif imc < IMC[2] :
+        interpretation = INTERPRETATION[2]
+    elif imc < IMC[3] :
+        interpretation = INTERPRETATION[3]
+    elif imc < IMC[4] :
+        interpretation = INTERPRETATION[4]
+    elif imc < IMC[5] :
+        interpretation = INTERPRETATION[5]
+    elif imc > IMC[5] :
+        interpretation = INTERPRETATION[6]
+    
+    return interpretation
+
+def test_imc(imc):
+    print("L'interprétation de votre IMC est : '" + message_imc(imc) + "'")
+
+
+test_imc(19)
+test_imc(16)
