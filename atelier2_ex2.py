70dfb101475fb9ae8db63792a55df00405a7a382
evafigueiredo8 <144215024+evafigueiredo8@users.noreply.github.com>
2023-09-08 11:09:20 +0200

    Add files via upload

--- atelier2_ex2.py
+++ atelier2_ex2.py
@@ -1,10 +1,21 @@
 import math
 
-def position(lst = list,elt = int) -> int :
+def position1(lst = list,elt = int) -> int :
     indice = -1
     for i in range (len(lst)) :
         if elt == lst[i] :
             indice = i
     return indice
 
-print(position([2,5,3,4,6,9],4))
+print(position1([2,5,3,4,6,9],4))
+
+def position2(lst = list,elt = int) -> int :
+    indice = -1
+    i = 0
+    while elt != lst[i] :
+        i += 1
+    if elt == lst[i] :
+        indice = i
+    return indice
+
+print(position2([2,5,3,4,6,9],4))
